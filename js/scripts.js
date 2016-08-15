192dbd22116385045a3e44d009e8a0f7f66c4a82
Jonathan Bucher and vichitra Pool <jonathan+vichitra@>
2016-08-15 14:38:03 -0700

    added practicing loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,36 @@
+$(document).ready(function(){
+  $(".button1").click(function(){
+    var fname = $("input#fname").val();
+    var lname = $("input#lname").val();
+    var fav1 = $("input#fav1").val();
+    var fav2 = $("input#fav2").val();
+    var fav3 = $("input#fav3").val();
 
-/*this funtion is for showing the age select.*/
+    var things = [fname, lname, fav1, fav2, fav3];
+    var complexArray = [things[1],things[2],things[0], things[3],things[4]];
+    complexArray.push(things);
 
-var ageWhileLoopYoung = function() {
-  var i = 25;
-  while ( i < 59 ) {
-    document.write("<option>"+i+"</option>");
-    i++;
-  };
-};
+    $(".putArrayHere").append("<li>"+things[0]+"</li>");
+    $(".putArrayHere").append("<li>"+things[1]+"</li>");
+    $(".putArrayHere").append("<li>"+things[2]+"</li>");
+    $(".putArrayHere").append("<li>"+things[3]+"</li>");
+    $(".putArrayHere").append("<li>"+things[4]+"</li>");
+  });
 
-var ageWhileLoopOld = function() {
-  var o = 26;
-  while ( o < 60 ) {
-    document.write("<option>"+o+"</option>");
-    o++;
-  };
-};
-
-
-
-
-$(function(){
-  $(".submitIt").click(function(event){
-    var name = $("#name").val();
-    var sexUser = $("input:radio[name=sexUser]:checked").val();
-    var sexWant = $("input:radio[name=sexWant]:checked").val();
-    var ageWantYoung = parseInt($(".ageWantYoung").val());
-    var ageWantOld = parseInt($(".ageWantOld").val());
+  $(".button2").click(function(){
+    var userInputs = ['fname', 'lname', 'fav1', 'fav2', 'fav3'];
 
 
-    //var error = 1;
-    $(".hideFirst").show();
-    $(".text-danger").text("");
-    $(".anderson, .taylor, .ellen, .justin").hide();
+    userInputs.forEach(function(string){
+      var userInputVar = $("#"+string).val();
+      $(".putArrayHere").append('<li class="newClass">'+userInputVar+'</li>');
+    });
 
-    if (!name) {
-      $(".text-danger").append("<p>Please enter your name.</p>");
-      //error = 2;
-    } else {
-      $(".name").show();
-      $(".name").text(name);
 
-    }
-    if (!sexUser) {
-      $(".text-danger").append("<p>Please enter your sex.</p>");
-      //error = 2;
-    }
-    if (!sexWant) {
-      $(".text-danger").append("<p>Please enter sex preference.</p>");
-      //error = 2;
-    }
 
-    if ( sexUser === "male" && sexWant === "female") {
-      if (ageWantYoung > 27 ) {
-        $(".text-danger").append("<p>All candidates are younger.</p>");
-      } else if (ageWantOld < 27 ) {
-        $(".text-danger").append("<p>All candidates are older.</p>");
-      } else {
-      $(".taylor").show();
-      }
-    }
-    if ( sexUser === "male" && sexWant === "male") {
-      if (ageWantYoung > 49 ) {
-        $(".text-danger").append("<p>All candidates are younger.</p>");
-      } else if (ageWantOld < 49 ) {
-        $(".text-danger").append("<p>All candidates are older.</p>");
-      } else {
-      $(".anderson").show();
-    }
-    }
-    if ( sexUser === "female" && sexWant === "male") {
-      if (ageWantYoung > 32 ) {
-        $(".text-danger").append("<p>All candidates are younger.</p>");
-      } else if (ageWantOld < 32 ) {
-        $(".text-danger").append("<p>All candidates are older.</p>");
-      } else {
-      $(".justin").show();
-      }
-    }
-    if ( sexUser === "female" && sexWant === "female") {
-      if (ageWantYoung > 58 ) {
-        $(".text-danger").append("<p>All candidates are younger.</p>");
-      } else if (ageWantOld < 58 ) {
-        $(".text-danger").append("<p>All candidates are older.</p>");
-      } else {
-      $(".ellen").show();
-      }
-    }
-
-    event.preventDefault();
   });
 });
+
+  var myNewFunction = function (Paremeter) {
+  var theVar = parameter;
+}
